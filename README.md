a164eac291001d486c19e048b10d8993395126a4
Harshit Pachar <harsitpachar@gmail.com>
2022-08-14 18:54:15 +0530

    final commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# IITISoC-22-Web-30-Alumni_Portal
-Team Web 30's IITISoC repository. Alumni Portal: A web app for the Alumni Cell, IIT Indore
--- client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import UserReducer from "./UserReducer";
-
-import { combineReducers } from "redux";
-
-const allReducers = combineReducers({
-    user: UserReducer
-});
-
-export default allReducers;
